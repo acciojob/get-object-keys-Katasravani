a4b98041d5ba29606445b0f2c39186de6a792649
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-12-12 11:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Define the student object
+const student = {
+  name: "John",
+};
+
+// Add getKeys method to Object prototype
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+// Example usage
+const keys = student.getKeys();
+console.log(keys); // Output: ['name']
+
